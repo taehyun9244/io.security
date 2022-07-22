5facc8104e8aa5b1e19b1d619cd483986d8f3fc5
taehyun9244 <namtaehyun9244@gmail.com>
2022-07-22 23:31:34 +0900

    start

--- src/main/java/com/example/basicsecurity/SecurityController.java
+++ src/main/java/com/example/basicsecurity/SecurityController.java
@@ -1,7 +1,13 @@
 package com.example.basicsecurity;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecurityController {
+
+    @GetMapping("/")
+    public String index(){
+        return "home";
+    }
 }
