cde27d15279f0d8489d8ee86d78a49b045cf492d
taehyun9244 <namtaehyun9244@gmail.com>
2022-08-27 12:44:00 +0900

    CustomUserDetailsService 구현

---
+++ src/main/java/com/example/basicsecurity/security/service/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.example.basicsecurity.security.service;
+
+import com.example.basicsecurity.domain.Account;
+import com.example.basicsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service("userDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Account account = userRepository.findByUsername(username);
+
+        if (account == null){
+            throw new UsernameNotFoundException("UsernameNotFoundException");
+        }
+
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(account.getRole()));
+
+        AccountContext accountContext = new AccountContext(account, roles);
+
+        return accountContext;
+    }
+}
